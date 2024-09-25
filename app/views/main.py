8566409d66b31c8156547f66aab992d237a7c800
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2024-09-25 19:14:26 +0300

    fix tests

--- app/views/main.py
+++ app/views/main.py
@@ -8,7 +8,7 @@ main_blueprint = Blueprint("main", __name__)
 @main_blueprint.route("/")
 @login_required
 def index():
-    return render_template("index.html")
+    return render_template("landing.html")
 
 
 @main_blueprint.route("/no-content")
