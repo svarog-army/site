75fc59e937e58d111a4b59c31656df6ec7ea3538
Mykola <nchernov@gmail.com>
2024-09-25 19:16:25 +0300

    fix: svarog project

--- svarog/commands/__init__.py
+++ svarog/commands/__init__.py
@@ -5,6 +5,7 @@ from sqlalchemy import orm
 from svarog import models as m
 from svarog import db, forms
 from svarog import schema as s
+from config import CFG
 
 
 def init(app: Flask):
@@ -12,7 +13,7 @@ def init(app: Flask):
     @app.shell_context_processor
     def get_context():
         """Objects exposed here will be automatically available from the shell."""
-        return dict(app=app, db=db, m=m, f=forms, s=s, sa=sa, orm=orm)
+        return dict(app=app, db=db, m=m, f=forms, s=s, sa=sa, orm=orm, CFG=CFG)
 
     if app.config["ENV"] != "production":
 
