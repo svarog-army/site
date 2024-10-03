06314fc7b8e6687110a40803eb1fe185c23c2bf3
marynag <mgrabchak1109@gmail.com>
2024-10-03 10:49:56 +0300

    fix code format

--- svarog/views/cookie_policy.py
+++ svarog/views/cookie_policy.py
@@ -3,16 +3,13 @@ from flask import (
     render_template,
 )
 
-from flask_babel import _
 from svarog import forms as f
 
 cookie_policy_bp = Blueprint("cookie_policy", __name__, url_prefix="/cookie-policy")
+
 
 @cookie_policy_bp.route("/", methods=["GET"])
 def index():
     form = f.ApplicationForm()
 
-
     return render_template("cookie_policy.html", form=form)
-
-
