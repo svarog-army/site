9fb42ecf5f454ddcd6c23e449a286f4f8b3c1d61
marynag <mgrabchak1109@gmail.com>
2024-10-03 12:13:23 +0300

    replace cookie policy to multilingual blueprint

--- svarog/views/__init__.py
+++ svarog/views/__init__.py
@@ -5,4 +5,3 @@ from .user import bp as user_blueprint
 from .application import application_bp as application_blueprint
 from .multilingual import multilingual
 from .admin import admin_blueprint
-from .cookie_policy import cookie_policy_bp as cookie_policy_blueprint
