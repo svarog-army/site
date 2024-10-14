548b7018c747b5c47324bc2bf5793912253b2e5a
Taraslysak <lysak.ipr@gmail.com>
2024-10-14 09:58:56 +0300

    setting tag version to compose file

---
+++ set_image_version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Get the current veriosn from command line
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+# remove v from version
+version=$(echo $1 | sed 's/v//')
+
+echo "Setting version to $version"
+
+# Set the "image: simple2b/svarog-app:latest" with "image: simple2b/svarog-app:$1" in compose.yml
+sed -i '' "s|simple2b/svarog-app:.*|simple2b/svarog-app:$version|g" compose.yaml
+
