26bfd37f5649025c8b1569749defb3a5b92b63c6
Taraslysak <lysak.ipr@gmail.com>
2024-10-14 10:26:26 +0300

    fix set_image_version script to work in actions

--- set_image_version.sh
+++ set_image_version.sh
@@ -13,5 +13,5 @@ version=$(echo $1 | sed 's/v//')
 echo "Setting version to $version"
 
 # Set the "image: simple2b/svarog-app:latest" with "image: simple2b/svarog-app:$1" in compose.yml
-sed -i '' "s|simple2b/svarog-app:.*|simple2b/svarog-app:$version|g" compose.yaml
+sed -i "s|simple2b/svarog-app:.*|simple2b/svarog-app:$version|g" compose.yaml # this is for linux, won't work on mac
 
