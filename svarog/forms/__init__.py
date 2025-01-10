2e6253e6eb7c2986b8de4e853baa8b177fd8b30d
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2025-01-10 20:43:14 +0200

    forms

--- svarog/forms/__init__.py
+++ svarog/forms/__init__.py
@@ -3,3 +3,4 @@ from .auth import LoginForm, RegistrationForm, ForgotForm, ChangePasswordForm
 from .user import UserForm, NewUserForm
 from .application import ApplicationForm
 from .recruit import RecruitForm, StatusForm
+from .specialty import SpecialtyForm, NewSpecialtyForm
