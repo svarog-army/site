1a2d3b2300088bc36afcebff2e86efb4c9b1528e
kipod <m.chernov@usf.mil.gov.ua>
2025-12-09 15:20:52 +0200

    npm build

--- svarog/static/js/redirect.js
+++ svarog/static/js/redirect.js
@@ -1,17 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*************************!*\
-  !*** ./src/redirect.ts ***!
-  \*************************/
-// redirect flow
-var redirectTag = document.querySelector('#redirect-url');
-if (redirectTag) {
-    var redirectUrl = redirectTag.dataset.url;
-    if (redirectUrl) {
-        window.location.href = redirectUrl;
-    }
-}
-
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoianMvcmVkaXJlY3QuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxnQkFBZ0I7QUFDaEIsSUFBTSxXQUFXLEdBQW1CLFFBQVEsQ0FBQyxhQUFhLENBQUMsZUFBZSxDQUFDLENBQUM7QUFDNUUsSUFBSSxXQUFXLEVBQUU7SUFDZixJQUFNLFdBQVcsR0FBRyxXQUFXLENBQUMsT0FBTyxDQUFDLEdBQUcsQ0FBQztJQUM1QyxJQUFJLFdBQVcsRUFBRTtRQUNmLE1BQU0sQ0FBQyxRQUFRLENBQUMsSUFBSSxHQUFHLFdBQVcsQ0FBQztLQUNwQztDQUNGIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vc3RhdGljLy4vc3JjL3JlZGlyZWN0LnRzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIHJlZGlyZWN0IGZsb3dcbmNvbnN0IHJlZGlyZWN0VGFnOiBIVE1MRGl2RWxlbWVudCA9IGRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoJyNyZWRpcmVjdC11cmwnKTtcbmlmIChyZWRpcmVjdFRhZykge1xuICBjb25zdCByZWRpcmVjdFVybCA9IHJlZGlyZWN0VGFnLmRhdGFzZXQudXJsO1xuICBpZiAocmVkaXJlY3RVcmwpIHtcbiAgICB3aW5kb3cubG9jYXRpb24uaHJlZiA9IHJlZGlyZWN0VXJsO1xuICB9XG59XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+(()=>{var r=document.querySelector("#redirect-url");if(r){var e=r.dataset.url;e&&(window.location.href=e)}})();
